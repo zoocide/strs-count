34bd6b5d20bdc1e9ab23f8900a817854090c69c8
Alexander Smirnov <zoocide@gmail.com>
2016-12-14 21:38:00 +0300

    add Fortran test programs

---
+++ t-free.f90
@@ -0,0 +1,15 @@
+! useful chars = 11 + 8 + 3 + 7 + 16 + 5 + 3 = 53
+! useful lines = 7
+! tlines = 15
+program test
+! comment after C
+      ! comment
+integer c
+c = 0
+print * &
+, 'Hello, world!' ! after line comment
+
+c = c + 1
+!another comment
+end! end of program
+
---
+++ t.for
@@ -0,0 +1,15 @@
+! useful chars = 11 + 8 + 3 + 6 + 17 + 5 + 3 = 53
+! useful lines = 7
+! total lines = 15
+      program test
+c comment after C
+      ! comment
+      integer c
+      c = 0
+      print *
+     x, 'Hello, world!'                                                 after 72 comment
+
+      c = c + 1
+*another comment
+      end! end of program
+
