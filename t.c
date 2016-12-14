4598cbd307e71fd78f99f5accabf559a6c1466f5
Alexander Smirnov <zoocide@gmail.com>
2016-12-14 12:15:09 +0300

    add c-program for test

---
+++ t.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+// useful strings = 6
+// useful chars   = 17 + 27 + 1 + 26 + 8 + 1 = 80
+
+int main(int argc, char** argv)
+{
+  // print hello. Space in the string is also useful character.
+  printf("Hello, world!\n");
+  /* finish
+ the porgram
+*/  return 0;
+
+/*
+   another comment
+    */
+}
