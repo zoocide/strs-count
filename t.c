d76a5255431068e917e3ef782c3aa2aaf9b638e0
Alexander Smirnov <zoocide@gmail.com>
2016-12-14 21:36:02 +0300

    update t.c

--- t.c
+++ t.c
@@ -2,10 +2,12 @@
 
 // useful strings = 6
 // useful chars   = 17 + 27 + 1 + 26 + 8 + 1 = 80
+// lines = 19
 
 int main(int argc, char** argv)
 {
-  // print hello. Space in the string is also useful character.
+  // print hello. \
+     Space in the string is also useful character.
   printf("Hello, world!\n");
   /* finish
  the porgram
